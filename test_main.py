71b11c5091fa3335d48425bd50e57cfcf97afe1a
denyszamiatin <Celtic19771977>
2021-06-27 10:21:46 +0300

    Pytest fixtures

--- test_main.py
+++ test_main.py
@@ -1,12 +1,34 @@
+import pytest
+
 import main
 import player
 
 
-def test_sort_players():
-    A = player.Player('A')
-    B = player.Player('B')
-    C = player.Player('C')
-    D = player.Player('D')
+@pytest.fixture(scope='module')
+def A():
+    return player.Player('A')
+
+
+@pytest.fixture(scope='module')
+def B():
+    return player.Player('B')
+
+
+@pytest.fixture(scope='module')
+def C():
+    return player.Player('C')
+
+
+@pytest.fixture(scope='module')
+def D():
+    return player.Player('D')
+
+
+def test_sort_players(A, B, C, D):
     players = [A, B, C, D]
-    sorted_players = main.sort_players(players, players[2])
-    assert sorted_players == [C, D, A, B]
+    assert main.sort_players(players, C) == [C, D, A, B]
+
+
+def test_sort_players_start(A, B, C, D):
+    players = [A, B, C, D]
+    assert main.sort_players(players, A) == [A, B, C, D]
