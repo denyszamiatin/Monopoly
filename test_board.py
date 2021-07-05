d8eba071015250db8f24b8ef40e00bb6a84ba383
katafalk <katafalk2009@gmail.com>
2021-07-05 19:23:29 +0300

    Added test_board.py

---
+++ test_board.py
@@ -0,0 +1,40 @@
+import pytest
+import board
+
+
+@pytest.fixture(scope='module')
+def dice():
+    return 6
+
+
+def test_get_field_property():
+    field = board.get_field(1, ('Property', 'Mediterranean Avenue',
+                                'Purple', 60))
+    assert isinstance(field, board.PropertyField)
+
+
+def test_get_field_tax():
+    field = board.get_field(1, ('Tax', 'Income tax', 75))
+    assert isinstance(field, board.TaxField)
+
+
+def test_get_field_go():
+    field = board.get_field(1, ('GO',))
+    assert isinstance(field, board.GoField)
+
+
+def test_get_field_error():
+    with pytest.raises(AssertionError):
+        board.get_field(1, ('Some field', 'LALALALA', 'Purple', 60))
+
+
+def test_get_player_position_1(dice):
+    assert board.get_player_position(1, dice) == 7
+
+
+def test_get_player_position_39(dice):
+    assert board.get_player_position(39, dice) == 5
+
+
+def test_get_start_field():
+    assert board.get_start_field() == 0
